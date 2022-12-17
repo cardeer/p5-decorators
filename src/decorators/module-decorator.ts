6493a10d4a5249e427807bf4bb78f98294819355
Siwakorn <cardeer.dev@gmail.com>
2022-12-17 16:33:30 +0700

    fix: module decorator

--- src/decorators/module-decorator.ts
+++ src/decorators/module-decorator.ts
@@ -4,6 +4,10 @@ export function Module<T extends keyof p5>(type: T) {
   return function <T extends { new (...args: any[]): {} }>(constructor: T) {
     return class extends constructor {
       public type: string = type
+
+      constructor(...args: any[]) {
+        super(args)
+      }
     }
   }
 }
